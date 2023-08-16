da672de06b8ce86736adaefc414b6866eca0dc70
Thiago <thiago.silva@claravista.com.br>
2023-08-16 17:17:41 -0300

    Correção de criação de registro de erro de produto

--- src/utils/createProductError.ts
+++ src/utils/createProductError.ts
@@ -2,7 +2,7 @@ import model from "../mongoose/models/ProductError";
 import handleError from "./handleError";
 
 const createProductError = async (sku: number, status: number | null) => {
-  if (sku !== 404) {
+  if (status !== 404) {
     try {
       await model.findOneAndUpdate({ sku }, { sku, status }, { upsert: true });
     } catch (error) {
